188d8256507bab6a4fd4da362b09dc693e3780c9
IsisDvrMex <isisdvr@hotmail.com>
2020-06-20 18:46:04 -0500

    end of section 3

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,4 +1,7 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
+import NavBar from "../../features/nav/NavBar/NavBar";
+import { Container } from "semantic-ui-react";
 
 class App extends Component {
 	constructor(props) {
@@ -7,9 +10,12 @@ class App extends Component {
 	}
 	render() {
 		return (
-			<div>
-				<h1>Re-vents</h1>
-			</div>
+			<Fragment>
+				<NavBar />
+				<Container className="main">
+					<EventDashboard />
+				</Container>
+			</Fragment>
 		);
 	}
 }
